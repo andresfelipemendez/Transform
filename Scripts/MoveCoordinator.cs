24723afff2120805006d3d8489aae53eaee6abc4
Enrique_Perez_SalsaSP <eperez@salsasp.co>
2016-10-03 16:35:13 -0500

    Cleaned the move code a little bit, added the canvas to swit between tranfoms

--- Scripts/MoveCoordinator.cs
+++ Scripts/MoveCoordinator.cs
@@ -7,14 +7,70 @@ public class MoveCoordinator : MonoBehaviour {
 	public GameObject y;
 	public GameObject z;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	CapsuleCollider localcollider;
+	BoxCollider axisCollider;
+	Transform grandParent;
+	MoveCoordinator coordinator;
+	GameObject axis;
+
+	void Update()
+	{
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, 100)) 
+		{
+			Debug.DrawLine(ray.origin, hit.point);
+
+			if (Input.GetMouseButtonDown (0)) {
+				Debug.Log (hit.transform.name);
+			}
+
+			if (Input.GetMouseButtonDown (0)) 
+			{
+				var g = hit.transform.gameObject;
+
+				axis = hit.transform.gameObject;
+				ActiveAxis (axis.name);
+
+				localcollider = axis.GetComponent<CapsuleCollider> ();	
+				axisCollider = axis.GetComponent<BoxCollider> ();	
+				axisCollider = axis.GetComponent<BoxCollider> ();	
+				grandParent = transform.parent;
+
+				localcollider.enabled = false;
+				axisCollider.enabled = true;
+			}
+
+			if (Input.GetMouseButton (0)) 
+			{
+				Debug.Log (axis);
+				var gp = grandParent.localPosition;
+				var hp = hit.point;
+				var pos = gp;
+
+				switch (axis.name) {
+				case "x":
+					pos = new Vector3 (hp.x, gp.y, gp.z);
+					break;
+				case "y":
+					pos = new Vector3 (gp.x, hp.y, gp.z);
+					break;
+				case "z":
+					pos = new Vector3 (gp.x, gp.y, hp.z);
+					break;
+				}
+
+				grandParent.position = pos;
+			}
+
+			if (Input.GetMouseButtonUp (0)) 
+			{
+				Finish ();
+				localcollider.enabled = true;
+				axisCollider.enabled = false;
+			}
+		}
 	}
 
 	public void ActiveAxis(string axis) {
@@ -36,9 +92,11 @@ public class MoveCoordinator : MonoBehaviour {
 
 	public void Finish()
 	{
-		x.SetActive (true);
-		y.SetActive (true);
-		z.SetActive (true);
+		Debug.Log ("finish");
+		foreach (Transform child in transform) 
+		{
+			child.gameObject.SetActive (true);
+		}
 	}
 
 }
--- Scripts/MoveGizmo.cs
+++ Scripts/MoveGizmo.cs
@@ -25,49 +25,5 @@ public class MoveGizmo : MonoBehaviour
 		axisCollider.enabled = false;
 	}
 
-	void Update()
-	{
-		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		RaycastHit hit;
-		
-		if (Physics.Raycast(ray, out hit, 100)) 
-		{
-			Debug.DrawLine(ray.origin, hit.point);
 
-			if (Input.GetMouseButtonDown (0)) {
-				coordinator.ActiveAxis (axis);
-				localcollider.enabled = false;
-				axisCollider.enabled = true;
-			}
-			
-			if (Input.GetMouseButton (0)) 
-			{
-				Debug.Log (axis);
-				var gp = grandParent.localPosition;
-				var hp = hit.point;
-				var pos = gp;
-
-				switch (axis) {
-				case "x":
-					pos = new Vector3 (hp.x, gp.y, gp.z);
-					break;
-				case "y":
-					pos = new Vector3 (gp.x, hp.y, gp.z);
-					break;
-				case "z":
-					pos = new Vector3 (gp.x, gp.y, hp.z);
-					break;
-				}
-
-				grandParent.position = pos;
-			}
-
-			if (Input.GetMouseButtonUp (0)) 
-			{
-				coordinator.Finish ();
-				localcollider.enabled = true;
-				axisCollider.enabled = false;
-			}
-		}
-	}
 }
