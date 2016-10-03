f9df0c99a9fdf9339ec2c6f82ddedde65a49e631
Enrique_Perez_SalsaSP <eperez@salsasp.co>
2016-10-03 15:23:27 -0500

    rotated based on the siblings

---
+++ Scripts/MoveCoordinator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCoordinator : MonoBehaviour {
+
+	public GameObject x;
+	public GameObject y;
+	public GameObject z;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ActiveAxis(string axis) {
+		switch (axis) {
+		case "x":
+			y.SetActive (false);
+			z.SetActive (false);
+			break;
+		case "y":
+			x.SetActive (false);
+			z.SetActive (false);
+			break;
+		case "z":
+			x.SetActive (false);
+			y.SetActive (false);
+			break;
+		}
+	}
+
+	public void Finish()
+	{
+		x.SetActive (true);
+		y.SetActive (true);
+		z.SetActive (true);
+	}
+
+}
