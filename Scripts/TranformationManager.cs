cae3801cae77fc8b54bfd5bc497b75ae3857e9de
Andres Felipe Mendez <fmendez1212@gmail.com>
2016-10-04 08:57:51 -0500

    transform manager

---
+++ Scripts/TranformationManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TranformationManager : MonoBehaviour 
+{
+	public Button move;
+	public Button rotate;
+	public Button scale;
+	public ColorBlock cb = new ColorBlock ();
+	// Use this for initialization
+	void Start () {
+		var cb = new ColorBlock ();
+//		cb.normalColor = 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Move()
+	{
+		//		rotate.colors = cb;
+	}
+}
